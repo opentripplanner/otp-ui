8963f4914658d7be57905cb506c41c53d18121be
Evan Siroky <evan.siroky@yahoo.com>
2019-12-02 21:07:53 -0800

    Add some more storybook addons and package import alias

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 module.exports = function({ config }) {
     config.module.rules.push({
       test: /.story.js$/,
       loaders: [require.resolve('@storybook/addon-storysource/loader')],
       enforce: 'pre',
     });
-  
+
+    config.resolve.alias = {
+      '#': path.resolve(__dirname, '../packages')
+    };
+
     return config;
   };
