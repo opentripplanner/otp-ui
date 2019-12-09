2bfc98354e19cf2333ea7318264a9266c544791a
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2019-12-09 11:25:21 -0500

    improvement(from-to-location-picker): Support styling, custom button text, optional icons.

--- packages/from-to-location-picker/src/index.js
+++ packages/from-to-location-picker/src/index.js
@@ -1,29 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { FromLocationIcon, ToLocationIcon } from "icons";
+import LocationIcon from "@opentripplanner/location-icon";
 import { LocationPickerSpan, Button, FromToPickerSpan } from "./styled";
 
-const iconStyle = { height: "0.9em" };
+const iconSize = "0.9em";
 
-const FromToLocationPicker = ({ onFromClick, onToClick }) => {
+const FromToLocationPicker = ({
+  fromText = "From here",
+  toText = "To here",
+  showIcons = true,
+  onFromClick,
+  onToClick
+}) => {
   return (
     <FromToPickerSpan>
       <LocationPickerSpan>
-        <FromLocationIcon style={iconStyle} />
-        <Button onClick={onFromClick}>From here</Button>
+        {showIcons ? <LocationIcon type="from" size={iconSize} /> : null}
+        <Button onClick={onFromClick}>{fromText}</Button>
       </LocationPickerSpan>
       <LocationPickerSpan>
-        <ToLocationIcon style={iconStyle} />
-        <Button onClick={onToClick}>To here</Button>
+        {showIcons ? <LocationIcon type="to" size={iconSize} /> : null}
+        <Button onClick={onToClick}>{toText}</Button>
       </LocationPickerSpan>
     </FromToPickerSpan>
   );
 };
 
 FromToLocationPicker.propTypes = {
+  fromText: PropTypes.string,
+  toText: PropTypes.string,
+  showIcons: PropTypes.bool,
   onFromClick: PropTypes.func.isRequired,
   onToClick: PropTypes.func.isRequired
+};
+
+FromToLocationPicker.defaultProps = {
+  fromText: "From here",
+  toText: "To here",
+  showIcons: true
 };
 
 export default FromToLocationPicker;
