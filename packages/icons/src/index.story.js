084bfac31f3a28b0f50c592c79cef315025bb6c9
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2019-12-11 19:05:48 -0500

    fix(Delete unused icons package.):

--- packages/icons/src/index.story.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { ToLocationIcon, FromLocationIcon } from ".";
-
-export default { title: "Common-Icons" };
-
-export const from = () => <FromLocationIcon />;
-export const to = () => <ToLocationIcon />;
