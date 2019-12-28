c8dff6f15d1e13280cbb904713c2034d2d76a567
Frank Purcell <fxpurcell@gmail.com>
2019-12-27 21:56:08 -0800

    chore: clean up lint errors

--- packages/vehicles/src/index.story.js
+++ packages/vehicles/src/index.story.js
@@ -1,31 +1,22 @@
 import BaseMap from "@opentripplanner/base-map";
-import React, { Component } from "react";
-import { action } from "@storybook/addon-actions";
+import React from "react";
+import PropTypes from "prop-types";
 import { withA11y } from "@storybook/addon-a11y";
 import { withInfo } from "@storybook/addon-info";
 import { storiesOf } from "@storybook/react";
 
 import "@opentripplanner/base-map/assets/map.css";
 
-const center = [45.523092, -122.671202];
-
-class Example extends Component {
-  constructor() {
-    super();
-    this.state = {
-      stops: []
-    };
-  }
-
-  render() {
-    return (
-      <BaseMap center={center}>
-      </BaseMap>
-    );
-  }
+function Example(props) {
+  const { center } = props;
+  return <BaseMap center={center}></BaseMap>;
 }
+
+Example.propTypes = {
+  center: PropTypes.arrayOf(PropTypes.float).isRequired
+};
 
 storiesOf("Realtime Vehicles", module)
   .addDecorator(withA11y)
   .addDecorator(withInfo)
-  .add("by Route", () => <Example />);
+  .add("by Route", () => <Example center={[45.523092, -122.671202]} />);
