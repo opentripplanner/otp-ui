732908cac1870d539ef1d93cb730438bf9cc5e65
Frank Purcell <fxpurcell@gmail.com>
2019-12-30 21:27:10 +0000

    fix(storybook): clean up the animation updater

--- packages/vehicles/src/index.story.js
+++ packages/vehicles/src/index.story.js
@@ -1,9 +1,11 @@
-import BaseMap from "@opentripplanner/base-map";
 import React from "react";
 import PropTypes from "prop-types";
+
 import { withA11y } from "@storybook/addon-a11y";
 import { withInfo } from "@storybook/addon-info";
 import { storiesOf } from "@storybook/react";
+
+import BaseMap from "@opentripplanner/base-map";
 
 import Vehicles from "./vehicles";
 import "@opentripplanner/base-map/assets/map.css";
@@ -13,6 +15,9 @@ const all = require("../__mocks__/all.json");
 const v1 = require("../__mocks__/all1.json");
 const v2 = require("../__mocks__/all2.json");
 const v3 = require("../__mocks__/all3.json");
+
+const v = [v1, v2, v3];
+const portland = [45.523, -122.671];
 
 function Map(props) {
   const { center } = props;
@@ -29,38 +34,34 @@ Map.propTypes = {
   vehicles: PropTypes.arrayOf(PropTypes.shape({})).isRequired
 };
 
-const portland = [45.523, -122.671];
-
-function allExample(props) {
+function allExample() {
   const retVal = <Map center={portland} vehicles={all} />;
   return retVal;
 }
 
-function routeExample(props) {
+function routeExample() {
   const retVal = <Map center={portland} vehicles={line} />;
   return retVal;
 }
 
-function cp(a, d) {
-  for (let i = 0; i < a.length; i++) a.pop();
+function animateExample() {
+  const [vehicleData, setVehicleData] = React.useState(null);
 
-  for (let k = 0; k < d.length; k++) a.push(d[k]);
-}
+  // when state of vehicle data is null (new) set the data updates here
+  // this makes sure we only have 1 updater interval (else chaos ensues)
+  // NOTE: because we're setting state below, this function is going to get called multiple times by react
+  //       if we don't have the gate of vehicleData == null, then we'll get multiple setInterval calls
+  if (vehicleData == null) {
+    let i = 0;
+    setVehicleData(v[i]);
+    setInterval(() => {
+      i += 1;
+      if (i >= v.length) i = 0;
+      setVehicleData(v[i]);
+    }, 3000);
+  }
 
-function animateExample(props) {
-  const v = [v1, v2, v3];
-
-  let i = 0;
-  const vehicleData = [];
-  cp(vehicleData, v1);
-  setInterval(function() {
-    console.log(`${i} ${vehicleData.length}`);
-    i += 1;
-    if (i >= v.length) i = 0;
-    cp(vehicleData, v[i]);
-  }, 5000);
-
-  const retVal = <Map center={portland} vehicles={vehicleData} />;
+  const retVal = <Map center={portland} vehicles={vehicleData}></Map>;
   return retVal;
 }
 
