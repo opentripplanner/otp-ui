e2a2d17b54f93838f6bb43cb7400f0518d8cc249
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2020-01-03 11:50:50 -0500

    fix(ModeSelectorPanel): keep track of transit modes previously selected.

--- packages/settings-selector/src/mode-selector-panel.js
+++ packages/settings-selector/src/mode-selector-panel.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {
-  hasTransit,
-  isTransit
-} from "@opentripplanner/core-utils/lib/itinerary";
+import { isTransit } from "@opentripplanner/core-utils/lib/itinerary";
 
 import ModeSelector from "./mode-selector";
 import SubmodeSelector from "./submode-selector";
 import { getModeOptions, getTransitSubmodeOptions } from "./util";
 import commonModes from "./__mocks__/modes"; // FIXME: Replace with ref to configuration.
+
+const defaultTransitModes = [
+  "TRAM", // FIXME: Take these from the 'registered' transit modes in core-utils/itinerary.
+  "RAIL",
+  "BUS",
+  "GONDOLA"
+];
 
 export default class ModeSelectorPanel extends Component {
   constructor(props) {
@@ -17,72 +21,98 @@ export default class ModeSelectorPanel extends Component {
     const { selectedModes } = props;
 
     this.state = {
-      selectedModes
+      selectedModes,
+      selectedTransitModes: []
     };
   }
 
   mainModeChangeHandler = id => {
-    const newModes = id.split("+");
-    const finalModes =
-      newModes[0] === "TRANSIT"
-        ? [
-            "TRAM", // FIXME: Take these from the 'registered' transit modes in core-utils/itinerary.
-            "RAIL",
-            "BUS",
-            "GONDOLA",
-            newModes.length > 1 ? newModes[1] : "WALK"
-          ]
-        : newModes;
-
-    const { onChange } = this.props;
-    if (onChange) onChange(id);
-
-    this.setState({ selectedModes: finalModes.join(",") });
-  };
-
-  transitModeChangeHandler = id => {
     const { selectedModes } = this.state;
-    const selectedModesArray = selectedModes.split(",");
-    const idx = selectedModesArray.indexOf(id);
-
-    // If clicked mode is selected, then remove it, o/w add it.
-    // Leave at least one selected per newplanner.trimet.org.
-    if (idx >= 0) {
-      // Count transit modes remaining
-      const transitModeCount = selectedModesArray.filter(isTransit).length;
-
-      if (transitModeCount >= 2) selectedModesArray.splice(idx, 1);
-    } else {
-      selectedModesArray.push(id);
+    let { selectedTransitModes } = this.state;
+    const activeTransitModes = selectedModes.filter(isTransit);
+    if (selectedTransitModes.length === 0) {
+      selectedTransitModes = selectedTransitModes.concat(defaultTransitModes);
     }
 
     const { onChange } = this.props;
     if (onChange) onChange(id);
 
-    this.setState({ selectedModes: selectedModesArray.join(",") });
+    const newModes = id.split("+");
+    if (newModes[0] === "TRANSIT") {
+      // Add previously selected transit modes only if none were active.
+      const finalModes = (activeTransitModes.length > 0
+        ? activeTransitModes
+        : selectedTransitModes
+      ).concat(newModes.length > 1 ? [newModes[1]] : ["WALK"]);
+
+      this.setState({
+        selectedModes: finalModes
+      });
+    } else {
+      this.setState({
+        selectedModes: newModes
+      });
+    }
   };
+
+  transitModeChangeHandler = id => {
+    const { selectedModes } = this.state;
+    const newModesArray = this.toggleSubmode(id, selectedModes, isTransit);
+
+    const { onChange } = this.props;
+    if (onChange) onChange(id);
+
+    this.setState({
+      selectedModes: newModesArray,
+      selectedTransitModes: newModesArray.filter(isTransit)
+    });
+  };
+
+  toggleSubmode(id, submodes, filter = o => o) {
+    const newSubmodes = submodes.concat([]);
+    const idx = newSubmodes.indexOf(id);
+
+    // If clicked mode is selected, then remove it, o/w add it.
+    // Leave at least one selected, as in newplanner.trimet.org.
+    if (idx >= 0) {
+      const subset = newSubmodes.filter(filter);
+      if (subset.length >= 2) {
+        newSubmodes.splice(idx, 1);
+      }
+    } else {
+      newSubmodes.push(id);
+    }
+
+    return newSubmodes;
+  }
 
   render() {
     const { className } = this.props;
     const { selectedModes } = this.state;
-    const selectedModesArray = selectedModes.split(",");
 
-    const modeOptions = getModeOptions(commonModes, selectedModesArray);
-
-    const transitModes = getTransitSubmodeOptions(
-      commonModes,
-      selectedModesArray
-    );
+    const modeOptions = getModeOptions(commonModes, selectedModes);
+    const transitModes = getTransitSubmodeOptions(commonModes, selectedModes);
 
     return (
       <div className={className}>
         <ModeSelector
-          className={className}
           modes={modeOptions}
           onChange={this.mainModeChangeHandler}
         />
+
         <div>Travel Preferences</div>
-        {hasTransit(selectedModes) && (
+
+        {selectedModes.some(isTransit) && (
+          <div>
+            Use companies:
+            <SubmodeSelector
+              modes={transitModes}
+              onChange={this.transitModeChangeHandler}
+            />
+          </div>
+        )}
+
+        {selectedModes.some(isTransit) && (
           <div>
             Use:
             <SubmodeSelector
@@ -105,11 +135,11 @@ ModeSelectorPanel.propTypes = {
   /**
    * A comma-separated list of selected modes, no whitespaces (corresponds to the mode URL parameter).
    */
-  selectedModes: PropTypes.string
+  selectedModes: PropTypes.arrayOf(PropTypes.string)
 };
 
 ModeSelectorPanel.defaultProps = {
   className: null,
   onChange: null,
-  selectedModes: "WALK,TRAM,RAIL,BUS,GONDOLA"
+  selectedModes: defaultTransitModes
 };
