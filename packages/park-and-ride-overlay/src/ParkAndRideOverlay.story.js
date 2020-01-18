a08a4039fd56a53e55df45dfed1cebf8bc768365
Evan Siroky <evan.siroky@yahoo.com>
2020-01-17 20:59:26 -0800

    Use mocks instead of randomized park and rides

--- packages/park-and-ride-overlay/src/ParkAndRideOverlay.story.js
+++ packages/park-and-ride-overlay/src/ParkAndRideOverlay.story.js
@@ -6,27 +6,12 @@ import { withInfo } from "@storybook/addon-info";
 import { storiesOf } from "@storybook/react";
 
 import ParkAndRideOverlay from ".";
+import parkAndRideLocations from "../__mocks__/park-and-rides.json";
 
 import "@opentripplanner/base-map/assets/map.css";
 
-function randomRange(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function makeRandomParkAndRide(mapVal, idx) {
-  return {
-    name: `Park and Ride ${idx}`,
-    x: randomRange(-122.6, -122.75),
-    y: randomRange(45.507, 45.531)
-  };
-}
-
 const center = [45.518092, -122.671202];
 const zoom = 13;
-
-const parkAndRideLocations = Array(5)
-  .fill(null)
-  .map(makeRandomParkAndRide);
 
 storiesOf("ParkAndRideOverlay", module)
   .addDecorator(withA11y)
