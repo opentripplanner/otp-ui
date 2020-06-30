0760bfa9494201bbe465aa5c1b136735bb3c72d5
Landon Reed <landontreed@gmail.com>
2020-06-30 11:10:37 -0400

    fix(time): temp fix for bad tz import
    re #152

--- packages/core-utils/src/time.js
+++ packages/core-utils/src/time.js
@@ -1,5 +1,4 @@
-import moment from "moment";
-import "moment-timezone";
+import moment from "moment-timezone";
 
 // special constants for making sure the following date format is always sent to
 // OTP regardless of whatever the user has configured as the display format
@@ -95,9 +94,9 @@ export function getUserTimezone() {
  * Formats current time for use in OTP query
  * The conversion to the user's timezone is needed for testing purposes.
  */
-export function getCurrentTime() {
+export function getCurrentTime(timezone = getUserTimezone()) {
   return moment()
-    .tz(getUserTimezone())
+    .tz(timezone)
     .format(OTP_API_TIME_FORMAT);
 }
 
