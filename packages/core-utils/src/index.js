cec39ef6dde07f32b2b4bc3526b36c842bc220c7
Evan Siroky <evan.siroky@yahoo.com>
2020-07-20 16:52:10 -0700

    refactor(core-utils): introduce new util file for route stuff
    BREAKING CHANGE:
    
    moves util functions related to routes to a new util file

--- packages/core-utils/src/index.js
+++ packages/core-utils/src/index.js
@@ -3,6 +3,7 @@ import * as map from "./map";
 import * as profile from "./profile";
 import * as query from "./query";
 import * as queryParams from "./query-params";
+import * as route from "./route";
 import * as storage from "./storage";
 import * as time from "./time";
 import * as types from "./types";
@@ -14,6 +15,7 @@ const core = {
   profile,
   query,
   queryParams,
+  route,
   storage,
   time,
   types,
