be1cb78e7cf57f88d9848fcebb9cd899eb045621
Landon Reed <landontreed@gmail.com>
2020-08-21 10:29:36 -0400

    test(itin-utils): add getTransitFare test

--- packages/core-utils/src/__tests__/itinerary.js
+++ packages/core-utils/src/__tests__/itinerary.js
@@ -1,7 +1,42 @@
-import { isTransit } from "../itinerary";
+import { getTransitFare, isTransit } from "../itinerary";
 
 describe("util > itinerary", () => {
   it("isTransit should work", () => {
     expect(isTransit("CAR")).toBeFalsy();
+  });
+
+  it("getTransitFare should return defaults with missing fare", () => {
+    const { centsToString, dollarsToString, transitFare } = getTransitFare(
+      null
+    );
+    // Make sure cents to string and dollars to string return same result
+    expect(dollarsToString(1)).toEqual(centsToString(100));
+    // Snapshot tests
+    expect(centsToString(100)).toMatchSnapshot();
+    expect(dollarsToString(1)).toMatchSnapshot();
+    expect(transitFare).toMatchSnapshot();
+  });
+
+  it("getTransitFare should work with valid fare component", () => {
+    const fareComponent = {
+      currency: {
+        currency: "USD",
+        defaultFractionDigits: 2,
+        currencyCode: "USD",
+        symbol: "$"
+      },
+      cents: 575
+    };
+    const { centsToString, dollarsToString, transitFare } = getTransitFare(
+      fareComponent
+    );
+    // Make sure cents to string and dollars to string return same result
+    expect(dollarsToString(transitFare / 100)).toEqual(
+      centsToString(transitFare)
+    );
+    // Snapshot tests
+    expect(centsToString(transitFare)).toMatchSnapshot();
+    expect(dollarsToString(1)).toMatchSnapshot();
+    expect(transitFare).toMatchSnapshot();
   });
 });
