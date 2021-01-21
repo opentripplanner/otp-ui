b0637598c475270545c9bc5e72130ec9fc96a236
Landon Reed <landontreed@gmail.com>
2021-01-21 15:31:35 -0500

    fix(leg-icon.js): use default getCompanyIcon and add as prop
    fix opentripplanner/otp-ui#232

--- packages/icons/src/leg-icon.js
+++ packages/icons/src/leg-icon.js
@@ -2,37 +2,9 @@ import { legType } from "@opentripplanner/core-utils/lib/types";
 import PropTypes from "prop-types";
 import React from "react";
 
-import Biketown from "./companies/biketown-icon";
-import Bird from "./companies/bird-icon";
-import Bolt from "./companies/bolt-icon";
-import Car2go from "./companies/car2go-icon";
-import Gruv from "./companies/gruv-icon";
-import Lime from "./companies/lime-icon";
-import Lyft from "./companies/lyft-icon";
-import Razor from "./companies/razor-icon";
-import Shared from "./companies/shared-icon";
-import Spin from "./companies/spin-icon";
-import Uber from "./companies/uber-icon";
+import { getCompanyIcon as defaultGetCompanyIcon } from "./companies";
 
-const companyLookup = {
-  biketown: Biketown,
-  bird: Bird,
-  bolt: Bolt,
-  car2go: Car2go,
-  gruv: Gruv,
-  lime: Lime,
-  lyft: Lyft,
-  razor: Razor,
-  shared: Shared,
-  spin: Spin,
-  uber: Uber
-};
-
-function getCompanyIcon(name) {
-  return companyLookup[name.toLowerCase()];
-}
-
-const LegIcon = ({ leg, ModeIcon, ...props }) => {
+const LegIcon = ({ getCompanyIcon, leg, ModeIcon, ...props }) => {
   let iconStr = leg.mode;
   if (iconStr === "CAR" && leg.rentedCar) {
     iconStr = leg.from.networks[0];
@@ -48,9 +20,11 @@ const LegIcon = ({ leg, ModeIcon, ...props }) => {
     iconStr = leg.from.networks[0];
   }
 
-  // try to see if the iconStr has a matching company icon. If so, return that.
-  const CompanyIcon = getCompanyIcon(iconStr);
-  if (CompanyIcon) return <CompanyIcon {...props} />;
+  // Check if the iconStr has a matching company icon. If so, return that.
+  if (typeof getCompanyIcon === "function") {
+    const CompanyIcon = getCompanyIcon(iconStr);
+    if (CompanyIcon) return <CompanyIcon {...props} />;
+  }
 
   // Do this for P&R, K&R and TNC trips without company icon
   if (iconStr && iconStr.startsWith("CAR")) iconStr = "CAR";
@@ -59,8 +33,14 @@ const LegIcon = ({ leg, ModeIcon, ...props }) => {
 };
 
 LegIcon.propTypes = {
+  // Optional override function for deriving the company icon for a given leg.
+  getCompanyIcon: PropTypes.func,
   leg: legType.isRequired,
   ModeIcon: PropTypes.elementType.isRequired
+};
+
+LegIcon.defaultProps = {
+  getCompanyIcon: defaultGetCompanyIcon
 };
 
 export default LegIcon;
