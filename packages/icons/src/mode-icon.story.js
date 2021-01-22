c0689eae79e912b81df1a6f7c50421a93495b1ea
Landon Reed <landontreed@gmail.com>
2021-01-22 13:29:39 -0500

    refactor(icons): add leg and mode icon stories

---
+++ packages/icons/src/mode-icon.story.js
@@ -0,0 +1,27 @@
+import { transitModes } from "@opentripplanner/core-utils/lib/itinerary";
+import { select, withKnobs } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import React from "react";
+
+import * as Icons from ".";
+
+function Container({ children }) {
+  return <div style={{ width: 40 }}>{children}</div>;
+}
+
+const allStories = storiesOf("Mode Icons", module).addDecorator(withKnobs);
+
+const options = ["TRANSIT", "WALK", "BICYCLE", ...transitModes];
+const defaultValue = "TRANSIT";
+
+Object.keys(Icons).forEach(key => {
+  // Skip if not a mode icon.
+  if (!key.indexOf("ModeIcon") === -1) return;
+  const IconComponent = Icons[key];
+  const story = () => (
+    <Container>
+      <IconComponent mode={select("Mode", options, defaultValue)} />
+    </Container>
+  );
+  allStories.add(key, story);
+});
