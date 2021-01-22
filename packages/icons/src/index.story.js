a67f30dfc7adb8893ba0d5d593eb9f109cd86e02
Landon Reed <landontreed@gmail.com>
2021-01-22 11:33:30 -0500

    test(icons): add story for leg/mode icons

--- packages/icons/src/index.story.js
+++ packages/icons/src/index.story.js
@@ -9,22 +9,37 @@ function Container({ children }) {
 
 const allStories = storiesOf("Icons", module);
 
-const noStoryComponents = [
-  "ClassicLegIcon",
-  "ClassicModeIcon",
-  "LegIcon",
-  "StandardLegIcon",
-  "StandardModeIcon",
-  "TriMetLegIcon",
-  "TriMetModeIcon"
-];
+const bikeRentalItinerary = require("./__mocks__/bike-rental-gbfs.json");
 
-Object.keys(Icons).forEach(iconKey => {
-  if (noStoryComponents.indexOf(iconKey) > -1) return;
-  const Component = Icons[iconKey];
-  allStories.add(iconKey, () => (
-    <Container>
-      <Component />
-    </Container>
-  ));
+const [walkLeg, bikeRentalLeg] = bikeRentalItinerary.legs;
+
+Object.keys(Icons).forEach(key => {
+  // Determine if icon is leg or mode icon (special icons that require props).
+  const isLegIcon = key.indexOf("LegIcon") !== -1;
+  const isModeIcon = key.indexOf("ModeIcon") !== -1;
+  const isTriMet = key.indexOf("TriMet") !== -1;
+  // Use the associated mode icon for the given leg icon (defaulting to
+  // standard mode icon).
+  const ModeIcon = Icons[key.replace("Leg", "Mode")] || Icons.StandardModeIcon;
+  const props = isModeIcon
+    ? { mode: "TRANSIT" }
+    : isLegIcon
+    ? {
+        // Use the bike rental leg for the TriMet leg icon.
+        leg: isTriMet ? bikeRentalLeg : walkLeg,
+        ModeIcon
+      }
+    : {};
+  const IconComponent = Icons[key];
+  const story = () => {
+    const { leg, mode } = props;
+    const label = leg ? leg.mode : mode;
+    return (
+      <Container>
+        {label}
+        <IconComponent {...props} />
+      </Container>
+    );
+  };
+  allStories.add(key, story);
 });
