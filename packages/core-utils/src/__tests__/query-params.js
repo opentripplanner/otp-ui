938925abbd77886dadd6a304115d0215385639b0
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2021-07-30 10:30:32 -0400

    test(query-params): Add tests.

---
+++ packages/core-utils/src/__tests__/query-params.js
@@ -0,0 +1,27 @@
+import queryParams, { getCustomQueryParams } from "../query-params";
+
+describe("query-params", () => {
+  describe("getCustomQueryParams", () => {
+    it("should return queryParams itself if no customizations", () => {
+      expect(getCustomQueryParams()).toEqual(queryParams);
+    });
+
+    it("should return queryParams with customizations", () => {
+      const customizations = {
+        maxWalkDistance: "Max Walk Distance In Meters",
+        "maxWalkDistance.options": [
+          {
+            text: "200 m",
+            value: 100
+          },
+          {
+            text: "500 m",
+            value: 500
+          }
+        ]
+      };
+      const customizedQueryParams = getCustomQueryParams(customizations);
+      expect(customizedQueryParams).toMatchSnapshot();
+    });
+  });
+});
