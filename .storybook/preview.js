433fe345c9b424d4a00354df0a1552b3080d6da4
miles-grant-ibigroup <miles.grant@ibigroup.com>
2021-08-13 15:54:12 +0200

    refactor: add msw to mock support mocked network requests

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
 
+import { setupWorker } from "msw";
+import handlers from "../packages/location-field/src/mocks/handlers";
+
+// Only install worker when running in browser
+if (typeof global.process === 'undefined') {
+  const worker = setupWorker(...handlers);
+  worker.start()
+}
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
