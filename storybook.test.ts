fb9af7236d2ac829377f8cb9cbe7d9a7ac34537e
miles-grant-ibigroup <miles.grant@ibigroup.com>
2021-09-09 17:01:56 +0100

    refactor: add explanation for changed storyshot render

--- storybook.test.ts
+++ storybook.test.ts
@@ -4,6 +4,12 @@ import "jest-styled-components";
 initStoryshots({
   asyncJest: true,
   test: async ({ story, done }) => {
+    /* The default storyshot method renders components as if they
+    were being rendered server-side. While this is good for many components,
+    map-based components especially benefit from rendering as if they were 
+    being rendered by a client. This results in more detailed and useful 
+    snapshots. This method renders each story as if it were being rendered 
+    by a browser. */
     const jsx = await story.render();
     expect(jsx).toMatchSnapshot();
     done();
