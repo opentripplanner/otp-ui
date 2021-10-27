0fd129910ebec8802083c09242a4952cc97352a2
miles-grant-ibigroup <miles.grant@ibigroup.com>
2021-10-27 13:04:38 +0100

    refactor(trip-form/test-utils): remove default wheelchair checkbox

--- packages/trip-form/src/test-utils/modes.js
+++ packages/trip-form/src/test-utils/modes.js
@@ -128,12 +128,10 @@ const commonModes = {
   accessModes: [
     {
       mode: "WALK",
-      showWheelchairSetting: true,
       label: "Saunter"
     },
     {
       mode: "BICYCLE",
-      showWheelchairSetting: true,
       label: "Transit + Personal Bike"
     },
     {
@@ -148,12 +146,10 @@ const commonModes = {
     },
     {
       mode: "CAR_PARK",
-      showWheelchairSetting: true,
       label: "Park & Ride"
     },
     {
       mode: "CAR_HAIL",
-      showWheelchairSetting: true,
       label: "Transit + Uber",
       company: "Uber" // Optional.
       // If not set and the user selects this, "companies" should default to Uber.
