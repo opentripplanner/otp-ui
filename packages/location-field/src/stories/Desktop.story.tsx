4864d39e527d64b890b7bcc263c2c56e65ad5b7e
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-02-17 14:35:48 -0800

    refactor(location-field): add todo about overriden messages

--- packages/location-field/src/stories/Desktop.story.tsx
+++ packages/location-field/src/stories/Desktop.story.tsx
@@ -136,6 +136,7 @@ export const WithBadApiKeyHandlesBadAutocomplete = (): JSX.Element => (
   />
 );
 
+// TODO: add custom messages once that becomes easier with the react-intl plugin
 export const WithCustomResultColorsAndIcons = (): JSX.Element => (
   <LocationField
     currentPosition={currentPosition}
