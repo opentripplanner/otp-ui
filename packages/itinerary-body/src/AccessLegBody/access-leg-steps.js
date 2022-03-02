701347244d5d3001df136872c306b8b79f1ced97
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-03-02 16:32:04 -0500

    feat(AccessLegSteps): Add basic localization support.

--- packages/itinerary-body/src/AccessLegBody/access-leg-steps.js
+++ packages/itinerary-body/src/AccessLegBody/access-leg-steps.js
@@ -1,54 +1,64 @@
 import coreUtils from "@opentripplanner/core-utils";
 import { DirectionIcon } from "@opentripplanner/icons";
-import React from "react";
-import PropTypes from "prop-types";
+import React, { ReactElement } from "react";
+import { FormattedMessage } from "react-intl";
+import { Step } from "@opentripplanner/types";
 
 import * as Styled from "../styled";
 import MapillaryButton from "./mapillary-button";
 
+interface Props {
+  mapillaryCallback?: (id: string) => void;
+  mapillaryKey?: string;
+  steps: Step[];
+}
+
+/**
+ * Applies the appropriate style for street names.
+ */
+function renderStreetName(streetName) {
+  return <Styled.StepStreetName>{streetName}</Styled.StepStreetName>;
+}
+
+/**
+ * Renders a turn-by-turn step of an access leg.
+ */
 export default function AccessLegSteps({
   steps,
   mapillaryCallback,
   mapillaryKey
-}) {
+}: Props): ReactElement {
   return (
     <Styled.Steps>
-      {steps.map((step, k) => {
-        return (
-          <Styled.StepRow key={k}>
-            <Styled.StepIconContainer>
-              <DirectionIcon relativeDirection={step.relativeDirection} />
-            </Styled.StepIconContainer>
+      {steps.map((step, k) => (
+        <Styled.StepRow key={k}>
+          <Styled.StepIconContainer>
+            <DirectionIcon relativeDirection={step.relativeDirection} />
+          </Styled.StepIconContainer>
 
-            <Styled.StepDescriptionContainer>
-              {coreUtils.itinerary.getStepDirection(step)}
-              <span>
-                {step.relativeDirection === "ELEVATOR" ? " to " : " on "}
-              </span>
-              <Styled.StepStreetName>
-                {coreUtils.itinerary.getStepStreetName(step)}
-              </Styled.StepStreetName>
-              <MapillaryButton
-                clickCallback={mapillaryCallback}
-                coords={step}
-                mapillaryKey={mapillaryKey}
-                padLeft
-              />
-            </Styled.StepDescriptionContainer>
-          </Styled.StepRow>
-        );
-      })}
+          <Styled.StepDescriptionContainer>
+            <FormattedMessage
+              description="Describes a step of the directions to reach a destination."
+              id="otpUi.AccessLegBody.instruction"
+              values={{
+                // FIXME: Include absolute dirs in translations.
+                absoluteDirection: step.absoluteDirection,
+                // FIXME: Include "unnamed roads/paths" in translations.
+                destination: coreUtils.itinerary.getStepStreetName(step),
+                relativeDirection: step.relativeDirection,
+                strong: renderStreetName
+              }}
+            />
+
+            <MapillaryButton
+              clickCallback={mapillaryCallback}
+              coords={step}
+              mapillaryKey={mapillaryKey}
+              padLeft
+            />
+          </Styled.StepDescriptionContainer>
+        </Styled.StepRow>
+      ))}
     </Styled.Steps>
   );
 }
-
-AccessLegSteps.propTypes = {
-  steps: coreUtils.types.stepsType.isRequired,
-  mapillaryCallback: PropTypes.func,
-  mapillaryKey: PropTypes.string
-};
-
-AccessLegSteps.defaultProps = {
-  mapillaryCallback: null,
-  mapillaryKey: null
-};
