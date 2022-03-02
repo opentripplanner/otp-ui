8e8d45cc9e121ac19fd8eeca39cf101c0afe660a
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-03-02 11:08:17 -0500

    test(storybook): Remove redundant data from snapshots.

--- .storybook/react-intl.js
+++ .storybook/react-intl.js
@@ -1,5 +1,12 @@
 import flatten from "flat";
 
+/**
+ * Copied from https://stackoverflow.com/questions/50940640/how-to-determine-if-jest-is-running-the-code-or-not
+ */
+function isRunningJest() {
+    return process.env.JEST_WORKER_ID !== undefined;
+}
+
 /** Locales supported in the storybook "Globe" dropdown menu. */
 const locales = ["en-US", "fr", "unknown"];
 
@@ -12,20 +19,22 @@ const packages = [
 
 /** Messages for all packages AND locales above. */
 const messages = {};
-packages.forEach((pkg) => {
-  locales.forEach((locale) => {
-    try {
-      messages[locale] = {
-        ...messages.[locale],
-        ...flatten(require(`../packages/${pkg}/i18n/${locale}.yml`))
-      };
-    } catch (e) {
-      // There is no yml files for the "unknown" locale,
-      // so it should fail and we won't display a message for that case.
-      if (locale !== "unknown") console.error(e);
-    }    
+if (!isRunningJest()) {
+  packages.forEach((pkg) => {
+    locales.forEach((locale) => {
+      try {
+        messages[locale] = {
+          ...messages.[locale],
+          ...flatten(require(`../packages/${pkg}/i18n/${locale}.yml`))
+        };
+      } catch (e) {
+        // There is no yml files for the "unknown" locale,
+        // so it should fail and we won't display a message for that case.
+        if (locale !== "unknown") console.error(e);
+      }    
+    });
   });
-});
+}
 
 // TODO place any (date, time, etc) format parameters here.
 const formats = {};
