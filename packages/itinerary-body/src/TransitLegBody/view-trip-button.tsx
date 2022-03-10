2ef305f7062bc1caee53c754e14d32f531adca50
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-03-10 16:50:36 -0500

    refactor(ViewTripButton): Add localization support.

---
+++ packages/itinerary-body/src/TransitLegBody/view-trip-button.tsx
@@ -0,0 +1,36 @@
+import React, { Component, ReactElement } from "react";
+import { FormattedMessage } from "react-intl";
+
+import * as S from "../styled";
+import { defaultMessages } from "../util";
+
+interface TripSection {
+  fromIndex: number;
+  toIndex: number;
+  tripId: string;
+}
+
+type Props = TripSection & {
+  setViewedTrip: (tripSection: TripSection) => void;
+};
+
+class ViewTripButton extends Component<Props> {
+  onClick = (): void => {
+    const { fromIndex, setViewedTrip, toIndex, tripId } = this.props;
+    setViewedTrip({ fromIndex, toIndex, tripId });
+  };
+
+  render(): ReactElement {
+    return (
+      <S.ViewerButton onClick={this.onClick} type="button">
+        <FormattedMessage
+          defaultMessage={defaultMessages["otpUi.TransitLegBody.tripViewer"]}
+          description="Link text to the trip viewer"
+          id="otpUi.TransitLegBody.tripViewer"
+        />
+      </S.ViewerButton>
+    );
+  }
+}
+
+export default ViewTripButton;
