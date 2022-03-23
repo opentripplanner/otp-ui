ce9e970732dc074628cf8d9093343a8a74758b7a
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-03-23 12:17:41 -0400

    refactor(ViewTextButton): Remove unused props

--- packages/itinerary-body/src/otp-react-redux/view-stop-button.tsx
+++ packages/itinerary-body/src/otp-react-redux/view-stop-button.tsx
@@ -7,7 +7,6 @@ import { defaultMessages } from "../util";
 interface Props {
   onStopClick: ({ stopId: string }) => void;
   stopId: string;
-  text?: string;
 }
 
 export default class ViewStopButton extends Component<Props> {
@@ -17,16 +16,13 @@ export default class ViewStopButton extends Component<Props> {
   };
 
   render(): ReactElement {
-    const { text } = this.props;
     return (
       <S.ViewerButton onClick={this.onClick}>
-        {text || (
-          <FormattedMessage
-            defaultMessage={defaultMessages["otpUi.TransitLegBody.stopViewer"]}
-            description="Text for link that opens the stop viewer"
-            id="otpUi.TransitLegBody.stopViewer"
-          />
-        )}
+        <FormattedMessage
+          defaultMessage={defaultMessages["otpUi.TransitLegBody.stopViewer"]}
+          description="Text for link that opens the stop viewer"
+          id="otpUi.TransitLegBody.stopViewer"
+        />
       </S.ViewerButton>
     );
   }
