e41209cec58358b82bea56577f796457275d9dcf
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-04-01 13:57:30 -0700

    refactor(core-utils): remove messages
    BREAKING CHANGE
    
    messages are removed from core-utils

--- packages/core-utils/src/index.ts
+++ packages/core-utils/src/index.ts
@@ -1,6 +1,5 @@
 import * as itinerary from "./itinerary";
 import * as map from "./map";
-import * as messages from "./messages";
 import * as profile from "./profile";
 import * as query from "./query";
 import * as queryParams from "./query-params";
@@ -12,7 +11,6 @@ import * as ui from "./ui";
 const core = {
   itinerary,
   map,
-  messages,
   profile,
   query,
   queryParams,
