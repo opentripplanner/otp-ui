96c4cad3157c51de1b7addf507722bcbf6e09fae
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-04-14 15:56:56 -0400

    refactor(trip-form): clash-proof unique ids

--- packages/trip-form/src/DateTimeSelector/index.tsx
+++ packages/trip-form/src/DateTimeSelector/index.tsx
@@ -220,6 +220,7 @@ export default function DateTimeSelector({
         {departureOptions.map(opt => (
           <ModeButton
             key={opt.type}
+            uniqueId={opt.type}
             onClick={setDepartArrive(opt)}
             selected={opt.isSelected}
           >
