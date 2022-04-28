dd204c411c281a336f7f0a29b3010fc0b75391aa
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-04-28 08:02:59 -0400

    refactor(DefaultStopMarker): Remove irrelevant comment

--- packages/stops-overlay/src/default-stop-marker.js
+++ packages/stops-overlay/src/default-stop-marker.js
@@ -16,9 +16,7 @@ import defaultEnglishMessages from "../i18n/en-US.yml";
 // the YAML loaders behave differently between webpack and our version of jest:
 // - the yaml loader for webpack returns a nested object,
 // - the yaml loader for jest returns messages with flattened ids.
-export const defaultMessages /* : Record<string, string> */ = flatten(
-  defaultEnglishMessages
-);
+export const defaultMessages = flatten(defaultEnglishMessages);
 
 const { leafletPathType, stopLayerStopType } = coreUtils.types;
 
