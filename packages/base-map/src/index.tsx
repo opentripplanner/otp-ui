4d6f0cd4c69bac6730696b1ce59179fd0b496d5f
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-06-03 11:33:17 -0700

    refactor(base-map): complete leaflet transition

--- packages/base-map/src/index.tsx
+++ packages/base-map/src/index.tsx
@@ -5,20 +5,21 @@ import maplibregl, { Event } from "maplibre-gl";
 
 import * as Styled from "./styled";
 /**
- * The BaseMap component renders a Leaflet map with overlays and other ad-hoc
+ * The BaseMap component renders a MapLibre map
  * markers that are declared as child elements of the BaseMap element.
  *
- * Overlays are groups of similar React-Leaflet markers, e.g. vehicle location
+ * Overlays are groups of similar MapLibre markers, e.g. vehicle location
  * markers, bus stop markers, etc.
+ *
+ * TODO:
  * Overlays are automatically added to the overlay control displayed by the
  * BaseMap. The user uses that control to turn overlays on or off.
- * See the
- * [Two Overlays From TriMet Transit Components](./?path=/story/basemap--with-two-overlays-from-trimet-transit-components)
- * example for more information on overlays.
  */
 type Props = {
-  maxZoom?: number;
   baseLayer?: string;
+  center?: [number, number];
+  children?: JSX.Element | JSX.Element[];
+  maxZoom?: number;
   onClick?: (evt: Event) => void;
   // Unknown is used here becuase of a maplibre/mapbox issue with the true type, MapLayerMouseEvent
   onContextMenu?: (e: unknown) => void;
@@ -32,17 +33,15 @@ type Props = {
     zoom: number;
   }) => void;
   zoom?: number;
-  center?: [number, number];
-  children?: JSX.Element | JSX.Element[];
 };
 const BaseMap = ({
-  maxZoom,
-  children,
   baseLayer = "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
   center,
+  children,
+  maxZoom,
+  onClick,
   onContextMenu,
   onViewportChanged,
-  onClick,
   zoom: initZoom = 12
 }: Props): JSX.Element => {
   const [viewState, setViewState] = React.useState({
@@ -50,6 +49,7 @@ const BaseMap = ({
     longitude: center?.[1],
     zoom: initZoom
   });
+
   useEffect(() => {
     if (typeof onViewportChanged === "function") {
       onViewportChanged(viewState);
@@ -58,18 +58,17 @@ const BaseMap = ({
 
   return (
     <Map
-      mapLib={maplibregl}
-      longitude={viewState.longitude}
-      onContextMenu={onContextMenu}
-      onClick={onClick}
       latitude={viewState.latitude}
-      zoom={viewState.zoom}
-      // TODO: better way to set height and width?
-      style={{ display: "block", width: "100%", height: "90vh" }}
-      onMove={evt => setViewState(evt.viewState)}
-      // mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      longitude={viewState.longitude}
+      mapLib={maplibregl}
       mapStyle={baseLayer}
       maxZoom={maxZoom}
+      onClick={onClick}
+      onContextMenu={onContextMenu}
+      onMove={evt => setViewState(evt.viewState)}
+      // TODO: better way to set height and width?
+      style={{ display: "block", width: "100%", height: "90vh" }}
+      zoom={viewState.zoom}
     >
       {children}
     </Map>
