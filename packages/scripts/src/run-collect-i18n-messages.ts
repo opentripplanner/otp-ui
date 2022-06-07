2b95f473682256c809dbf96273a840c41ca1e3a2
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-06-07 11:23:55 -0400

    chore(package.json): Use run-* in package script, update examples.

--- packages/scripts/src/run-collect-i18n-messages.ts
+++ packages/scripts/src/run-collect-i18n-messages.ts
@@ -3,9 +3,9 @@
  * and creates a CSV file with the id, description, and messages in the selected language(s).
  */
 // Example usage for all packages and all languages in this repo:
-//   node path-to/lib/collect-i18n-messages.js ../**/src ../**/i18n
+//   node path-to/lib/run-collect-i18n-messages.js ../**/src ../**/i18n
 // Example usage for all packages and one language in this repo:
-//   node path-to-lib/collect-i18n-messages.js ../**/src ../**/i18n/en-US.yml
+//   node path-to-lib/run-collect-i18n-messages.js ../**/src ../**/i18n/en-US.yml
 
 import collectI18nMessages from "./collect-i18n-messages";
 
