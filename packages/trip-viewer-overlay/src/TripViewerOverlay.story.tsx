9b4ae21e34c6ba674934c482767c5a4d7f8ffc31
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-06-10 10:25:45 -0700

    feat(trip-viewer-overlay): Add typescript, move to MapLibreGL
    BREAKING CHANGE: Removes Leaflet, adjusts path props

--- packages/trip-viewer-overlay/src/TripViewerOverlay.story.tsx
+++ packages/trip-viewer-overlay/src/TripViewerOverlay.story.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import tripData from "../__mocks__/mock-trip.json";
 import TripViewerOverlay from ".";
 
-import "../../../node_modules/leaflet/dist/leaflet.css";
+import "maplibre-gl/dist/maplibre-gl.css";
 
-const center = [45.518092, -122.671202];
+const center: [number, number] = [45.518092, -122.671202];
 const zoom = 13;
 
 export default {
@@ -14,18 +14,17 @@ export default {
   component: TripViewerOverlay
 };
 
-export const Default = () => (
+export const Default = (): JSX.Element => (
   <BaseMap center={center} zoom={zoom}>
     <TripViewerOverlay tripData={tripData} visible />
   </BaseMap>
 );
 
-export const WithPathStyling = () => (
+export const WithPathStyling = (): JSX.Element => (
   <BaseMap center={center} zoom={zoom}>
     <TripViewerOverlay
-      leafletPath={{
+      path={{
         color: "#000",
-        dashArray: "2 3 5 7 11",
         opacity: 0.2,
         weight: 5
       }}
