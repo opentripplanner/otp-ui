bf052825dd7c3a5d2c9e4b797a262b06750c1443
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-08-03 09:21:16 -0400

    test(core-utils): Add more itinerary assertions.

--- packages/core-utils/src/__tests__/itinerary.js
+++ packages/core-utils/src/__tests__/itinerary.js
@@ -9,44 +9,55 @@ const bikeRentalItinerary = require("./__mocks__/bike-rental-itinerary.json");
 const tncItinerary = require("./__mocks__/tnc-itinerary.json");
 
 describe("util > itinerary", () => {
-  it("isTransit should work", () => {
-    expect(isTransit("CAR")).toBeFalsy();
+  describe("isTransit", () => {
+    it("should work", () => {
+      expect(isTransit("CAR")).toBeFalsy();
+      expect(isTransit("BUS")).toBeTruthy();
+    });
   });
 
-  it("getCompanyFromLeg should return company for bike rental leg", () => {
-    const company = getCompanyFromLeg(bikeRentalItinerary.legs[1]);
-    expect(company).toEqual("GBFS");
+  describe("getCompanyFromLeg", () => {
+    it("should return company for bike rental leg", () => {
+      const company = getCompanyFromLeg(bikeRentalItinerary.legs[1]);
+      expect(company).toEqual("GBFS");
+    });
+
+    it("should return company for TNC leg", () => {
+      const company = getCompanyFromLeg(tncItinerary.legs[0]);
+      expect(company).toEqual("UBER");
+    });
   });
 
-  it("getCompanyFromLeg should return company for TNC leg", () => {
-    const company = getCompanyFromLeg(tncItinerary.legs[0]);
-    expect(company).toEqual("UBER");
+  describe("getTransitFare", () => {
+    it("should return defaults with missing fare", () => {
+      const { transitFare } = getTransitFare(null);
+      // transit fare value should be zero
+      expect(transitFare).toMatchSnapshot();
+    });
+
+    it("should work with valid fare component", () => {
+      const fareComponent = {
+        currency: {
+          currency: "USD",
+          defaultFractionDigits: 2,
+          currencyCode: "USD",
+          symbol: "$"
+        },
+        cents: 575
+      };
+      const { currencyCode, transitFare } = getTransitFare(fareComponent);
+      expect(currencyCode).toEqual(fareComponent.currency.currencyCode);
+      // Snapshot tests
+      expect(transitFare).toMatchSnapshot();
+    });
   });
 
-  it("getTransitFare should return defaults with missing fare", () => {
-    const { transitFare } = getTransitFare(null);
-    // transit fare value should be zero
-    expect(transitFare).toMatchSnapshot();
-  });
-
-  it("getTransitFare should work with valid fare component", () => {
-    const fareComponent = {
-      currency: {
-        currency: "USD",
-        defaultFractionDigits: 2,
-        currencyCode: "USD",
-        symbol: "$"
-      },
-      cents: 575
-    };
-    const { currencyCode, transitFare } = getTransitFare(fareComponent);
-    expect(currencyCode).toEqual(fareComponent.currency.currencyCode);
-    // Snapshot tests
-    expect(transitFare).toMatchSnapshot();
-  });
-
-  it("calculateFare should return the correct currency code for TNC leg", () => {
-    const fareResult = calculateTncFares(tncItinerary, true);
-    expect(fareResult.currencyCode).toEqual("USD");
+  describe("calculateTncFares", () => {
+    it("should return the correct amounts and currency for an itinerary with TNC", () => {
+      const fareResult = calculateTncFares(tncItinerary, true);
+      expect(fareResult.currencyCode).toEqual("USD");
+      expect(fareResult.maxTNCFare).toEqual(19);
+      expect(fareResult.minTNCFare).toEqual(17);
+    });
   });
 });
