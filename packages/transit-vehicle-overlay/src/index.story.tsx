0400a41db5d43a4b840fdfb449c0c07e984126dd
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-10-20 15:43:37 -0400

    test(transit vehicle overlay): Update stories

--- packages/transit-vehicle-overlay/src/index.story.tsx
+++ packages/transit-vehicle-overlay/src/index.story.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 
-import vehicleData from "../__mocks__/tm_all.json";
+import seattleVehicleData from "../__mocks__/seattle.json";
+import trimetVehicleData from "../__mocks__/tm_all.json";
 import { withMap } from "../../../.storybook/base-map-wrapper";
 
 import TransitVehicleOverlay from ".";
 
 const PORTLAND: [number, number] = [45.523, -122.671];
+const SEATTLE: [number, number] = [47.6129432, -122.4821484];
 
-export const TransitVehicleOverlayExample = () => {
+export const TrimetExample = () => {
   return (
-    <TransitVehicleOverlay vehicles={[vehicleData.resultSet.vehicle[0]]} />
+    <TransitVehicleOverlay vehicles={trimetVehicleData.resultSet.vehicle} />
   );
 };
+TrimetExample.decorators = [withMap(PORTLAND, 8)];
+
+export const SeattleExample = () => {
+  return (
+    <TransitVehicleOverlay vehicles={seattleVehicleData.vehiclePositions} />
+  );
+};
+SeattleExample.decorators = [withMap(SEATTLE, 8)];
 
 export default {
   title: "TransitVehicleOverlay",
-  component: TransitVehicleOverlayExample,
-  decorators: [withMap(PORTLAND, 8)]
+  component: TransitVehicleOverlay
 };
