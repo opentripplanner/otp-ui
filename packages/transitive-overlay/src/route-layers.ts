62167dff332b194a765d28d84065275c4a9b9b3b
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-11-29 10:40:55 -0500

    fix(transitive-overlay): restore arc rendering for flex segments

--- packages/transitive-overlay/src/route-layers.ts
+++ packages/transitive-overlay/src/route-layers.ts
@@ -2,6 +2,13 @@ import polyline from "@mapbox/polyline";
 import { SymbolLayout } from "mapbox-gl";
 import { TransitivePattern, TransitiveRoute } from "@opentripplanner/types";
 
+import lineArc from "@turf/line-arc";
+import lineDistance from "@turf/line-distance";
+import midpoint from "@turf/midpoint";
+import destination from "@turf/destination";
+import bearing from "@turf/bearing";
+import distance from "@turf/distance";
+
 /**
  * Create a labeled-line feature for the given transit route pattern.
  */
@@ -32,16 +39,58 @@ export function patternToRouteFeature(
     .replace(/\s/g, "!")
     .replace(/[+-0124679FHJLPTVXYZ]/g, "E");
 
+  const properties = {
+    color: `#${route.route_color || "000080"}`,
+    name: routeName,
+    nameUpper: routeNameUpper,
+    routeType: route.route_type,
+    textColor: `#${route.route_text_color || "eee"}`,
+    type: "route"
+  };
+
+  const isFlex = pattern.stops[pattern.stops.length - 1].stop_id.includes(
+    "flexed_to"
+  );
+  const straight = polyline.toGeoJSON(polyline.encode(concatenatedLines));
+
+  if (isFlex) {
+    // Create clone of plain route that only includes first and last point
+    const extraStraight = { ...straight };
+    extraStraight.coordinates = [
+      extraStraight.coordinates[0],
+      extraStraight.coordinates[extraStraight.coordinates.length - 1]
+    ];
+
+    // Adapted from https://github.com/Turfjs/turf/issues/1218#issuecomment-592421977
+    const length = lineDistance(extraStraight, "kilometers");
+    const mp = midpoint(
+      extraStraight.coordinates[0],
+      extraStraight.coordinates[1]
+    );
+    const center = destination(
+      mp,
+      length,
+      bearing(extraStraight.coordinates[0], extraStraight.coordinates[1]) - 90
+    );
+
+    const arc = lineArc(
+      center,
+      distance(center, extraStraight.coordinates[0]),
+      bearing(center, extraStraight.coordinates[1]),
+      bearing(center, extraStraight.coordinates[0]),
+      { steps: 500 }
+    ).geometry;
+
+    return {
+      geometry: arc,
+      properties,
+      type: "Feature"
+    };
+  }
+
   return {
-    geometry: polyline.toGeoJSON(polyline.encode(concatenatedLines)),
-    properties: {
-      color: `#${route.route_color || "000080"}`,
-      name: routeName,
-      nameUpper: routeNameUpper,
-      routeType: route.route_type,
-      textColor: `#${route.route_text_color || "eee"}`,
-      type: "route"
-    },
+    geometry: straight,
+    properties,
     type: "Feature"
   };
 }
