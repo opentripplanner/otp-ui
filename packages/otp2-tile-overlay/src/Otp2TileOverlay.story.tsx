4a4e30ce7d3f2efd7d8e21486a3eb03d541674eb
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-12-20 10:09:57 -0500

    refactor(otp-tile-overlay): add description of how to use stories

--- packages/otp2-tile-overlay/src/Otp2TileOverlay.story.tsx
+++ packages/otp2-tile-overlay/src/Otp2TileOverlay.story.tsx
@@ -22,7 +22,9 @@ export const OtpTileLayer = (): JSX.Element => {
       <pre style={{ display: "inline", paddingLeft: 5 }}>
         https://fake-otp-server.com/otp
       </pre>{" "}
-      mocks downtown Philadelphia stop tile data.
+      mocks downtown Philadelphia stop tile data. Check stops in the layer
+      selector and zoom in on downtown Philadelphia (near city hall) to see some
+      stops loaded from OTP tiles.
       <BaseMap center={[0, 0]} zoom={3} style={{ height: "80vh" }}>
         {generateOTP2TileLayers(
           [{ type: "stops" }],
