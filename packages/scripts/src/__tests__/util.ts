15a773d2b9dfef2d22e2b1da9f394d519d8e49ee
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-05-17 15:47:57 -0400

    test(scripts): Add more tests for i18n validation.

--- packages/scripts/src/__tests__/util.ts
+++ packages/scripts/src/__tests__/util.ts
@@ -3,6 +3,8 @@ import { sortSourceAndYmlFiles } from "../util";
 // eslint-disable-next-line import/prefer-default-export
 export const mocksFolderFromCwd = `${process.cwd()}/packages/scripts/src/mocks`;
 
+const fromToPickerSrcFolder = "from-to-location-picker/src";
+
 describe("util", () => {
   describe("sortSourceAndYmlFiles", () => {
     it("should sort files when passed folders", async () => {
@@ -10,18 +12,39 @@ describe("util", () => {
         "node",
         "script-name.js",
         `${mocksFolderFromCwd}/i18n1`,
-        `${mocksFolderFromCwd}/packages/from-to-location-picker/src`
+        `${mocksFolderFromCwd}/../../../${fromToPickerSrcFolder}`
       ];
       const {
-        exceptionFiles
-        // sourceFiles,
-        // ymlFilesByLocale
+        exceptionFiles,
+        sourceFiles,
+        ymlFilesByLocale
       } = await sortSourceAndYmlFiles(args);
 
       expect(exceptionFiles.length).toBe(1);
       expect(exceptionFiles[0]).toBe(
         `${mocksFolderFromCwd}/i18n1/i18n-exceptions.json`
       );
+
+      const localeKeys = Object.keys(ymlFilesByLocale);
+      expect(localeKeys.length).toBe(2);
+      expect(localeKeys.includes("en-US")).toBe(true);
+      expect(localeKeys.includes("fr")).toBe(true);
+
+      const sourceFileList = [
+        "index.tsx",
+        "index.story.tsx",
+        "styled.ts",
+        "types.ts"
+      ];
+      expect(sourceFiles.length).toBe(sourceFileList.length);
+      sourceFiles.forEach(f => {
+        const fromToIndex = f.indexOf(fromToPickerSrcFolder);
+        expect(fromToIndex !== -1);
+        const trimmedName = f.substring(
+          fromToIndex + fromToPickerSrcFolder.length + 1
+        );
+        expect(sourceFileList.includes(trimmedName));
+      });
     });
   });
 });
