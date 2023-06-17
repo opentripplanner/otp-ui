85a519478065fab384110d9b72e9dd1a14f5ccef
Daniel Heppner <dh@danielheppner.com>
2023-06-16 17:42:01 -0700

    build: upgrade lerna

---
+++ scripts/build-esm.sh
@@ -0,0 +1,1 @@
+cross-env BABEL_ENV=esm babel --extensions '.js,.ts,.tsx' --ignore **/*.story.js,**/*.story.ts,**/*.story.d.ts,**/*.story.tsx,**/*.spec.js,**/*.spec.ts,**/*.test.js,**/*.test.ts,**/__tests__/**,**/__unpublished__/** --root-mode upward --source-maps true src -d esm
