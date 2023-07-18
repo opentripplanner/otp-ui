7b9ef063e26c9e9e5aba688eb63ace947fa1302c
Daniel Heppner <daniel.heppner@ibigroup.com>
2023-07-17 20:03:49 -0700

    build: remove unneeded flags

--- scripts/build-cjs.sh
+++ scripts/build-cjs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-babel --extensions '.js,.ts,.tsx' --ignore **/*.story.js,**/*.story.ts,**/*.story.d.ts,**/*.story.tsx,**/*.spec.js,**/*.spec.ts,**/*.test.js,**/*.test.ts,**/__tests__/**,**/__unpublished__/** --root-mode upward --source-maps true src -d lib --copy-files --no-copy-ignored
+babel --extensions '.js,.ts,.tsx' --ignore **/*.story.js,**/*.story.ts,**/*.story.d.ts,**/*.story.tsx,**/*.spec.js,**/*.spec.ts,**/*.test.js,**/*.test.ts,**/__tests__/**,**/__unpublished__/** --root-mode upward --source-maps true src -d lib
