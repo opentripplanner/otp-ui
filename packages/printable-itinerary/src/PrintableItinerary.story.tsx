6faf5847babaf0d91c828dd0c113315b71df35dd
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-09-14 14:03:35 -0400

    refactor(printable-itinerary): Revert converting legs to legacy for tnc itin.

--- packages/printable-itinerary/src/PrintableItinerary.story.tsx
+++ packages/printable-itinerary/src/PrintableItinerary.story.tsx
@@ -1,4 +1,3 @@
-import { convertGraphQLResponseToLegacy } from "@opentripplanner/core-utils/lib/itinerary";
 import { ClassicLegIcon, TriMetLegIcon } from "@opentripplanner/icons";
 import React from "react";
 import styled from "styled-components";
@@ -28,13 +27,6 @@ const StyledPrintableItinerary = styled(PrintableItinerary)`
     background-color: pink;
   }
 `;
-
-function withLegacyLegs(itinerary) {
-  return {
-    ...itinerary,
-    legs: itinerary.legs.map(convertGraphQLResponseToLegacy)
-  };
-}
 
 export default {
   title: "PrintableItinerary",
@@ -148,7 +140,7 @@ export const EScooterRentalTransitItinerary = () => (
 export const TncTransitItinerary = () => (
   <PrintableItinerary
     config={config}
-    itinerary={withLegacyLegs(tncTransitTncItinerary)}
+    itinerary={tncTransitTncItinerary}
     LegIcon={TriMetLegIcon}
   />
 );
