8490ec3bc34c6e25d12c20f487148296d5c47392
Rosales <RosalesA@tri-met.org>
2023-09-14 10:38:51 -0700

    fix: gracefully made default for RouteDescriptionFooter -undefined-

--- packages/itinerary-body/src/stories/itinerary-body-defaults-wrapper.tsx
+++ packages/itinerary-body/src/stories/itinerary-body-defaults-wrapper.tsx
@@ -49,7 +49,7 @@ export default class ItineraryBodyDefaultsWrapper extends Component<
       LineColumnContent,
       PlaceName,
       RouteDescription,
-      RouteDescriptionFooter,
+      RouteDescriptionFooter = undefined,
       showAgencyInfo,
       showLegIcon,
       showMapButtonColumn = true,
