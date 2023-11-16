e6fb5447f42cf3bd75bf6d2a3d857173b8f93b41
Daniel Heppner <daniel.heppner@ibigroup.com>
2023-11-16 01:41:34 +0100

    fix(stops-overlay): use StopEventHandler type

--- packages/stops-overlay/src/index.tsx
+++ packages/stops-overlay/src/index.tsx
@@ -1,5 +1,9 @@
 import { Popup } from "@opentripplanner/base-map";
-import { MapLocationActionArg, Stop } from "@opentripplanner/types";
+import {
+  MapLocationActionArg,
+  Stop,
+  StopEventHandler
+} from "@opentripplanner/types";
 import { EventData } from "mapbox-gl";
 import { Layer, Source, useMap } from "react-map-gl";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
@@ -48,7 +52,7 @@ type Props = {
   /**
    * A method fired when the stop viewer is opened in the popup
    */
-  setViewedStop?: (stop: Stop) => void;
+  setViewedStop?: StopEventHandler;
 };
 
 /**
