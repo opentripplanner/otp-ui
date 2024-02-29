2617f18430869fd387ed030e4f8c7ec19f0880e8
amy-corson-ibigroup <115499534+amy-corson-ibigroup@users.noreply.github.com>
2024-02-29 10:48:24 -0600

    Address PR feedback

--- packages/base-map/src/mapControlLocale.tsx
+++ packages/base-map/src/mapControlLocale.tsx
@@ -4,13 +4,6 @@ import { IntlShape } from "react-intl";
 // https://github.com/maplibre/maplibre-gl-js/blob/main/src/ui/default_locale.ts
 
 const MAP_CONTROL_TRANSLATION_KEYS = [
-  "AttributionControl.ToggleAttribution",
-  "AttributionControl.MapFeedback",
-  "FullscreenControl.Enter",
-  "FullscreenControl.Exit",
-  "GeolocateControl.FindMyLocation",
-  "GeolocateControl.LocationNotAvailable",
-  "LogoControl.Title",
   "NavigationControl.ResetBearing",
   "NavigationControl.ZoomIn",
   "NavigationControl.ZoomOut",
@@ -19,8 +12,6 @@ const MAP_CONTROL_TRANSLATION_KEYS = [
   "ScaleControl.Kilometers",
   "ScaleControl.Miles",
   "ScaleControl.NauticalMiles",
-  "TerrainControl.Enable",
-  "TerrainControl.Disable",
   "CooperativeGesturesHandler.WindowsHelpText",
   "CooperativeGesturesHandler.MacHelpText",
   "CooperativeGesturesHandler.MobileHelpText"
@@ -28,7 +19,7 @@ const MAP_CONTROL_TRANSLATION_KEYS = [
 
 const mapControlTranslationObject = (
   intl: IntlShape
-): { [key: string]: string } => {
+): Record<string, string> => {
   const languageObject = {};
 
   MAP_CONTROL_TRANSLATION_KEYS.forEach(x => {
