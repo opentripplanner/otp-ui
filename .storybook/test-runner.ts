c881a6eaf21faa69971c09e0ea89f249619e7620
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-04-05 23:15:29 +0800

    use mock ids for snapshots

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,11 +1,22 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
+import { injectAxe, checkA11y } from 'axe-playwright';
 
 const config: TestRunnerConfig = {
+  async preVisit(page) {
+    await injectAxe(page);
+  },
   async postVisit(page, context) {
     // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
     const elementHandler = await page.$('#storybook-root');
     const innerHTML = await elementHandler?.innerHTML();
     expect(innerHTML).toMatchSnapshot();
+
+    // await checkA11y(page, '#storybook-root', {
+    //   detailedReport: true,
+    //   detailedReportOptions: {
+    //     html: true,
+    //   },
+    // });
   },
 };
 
