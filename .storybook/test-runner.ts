879241a79c8dd34d7b8f584fbc75f11d16124471
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-04-08 18:29:01 +0800

    rerun snapshots with delay

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -7,6 +7,7 @@ const config: TestRunnerConfig = {
   },
   async postVisit(page, context) {
     // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const elementHandler = await page.$('#storybook-root');
     const innerHTML = await elementHandler?.innerHTML();
     expect(innerHTML).toMatchSnapshot();
