fde445633654bdd3b6d814a228f93ae80b2d3e62
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-04-10 21:51:01 +0800

    add a11y test

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,5 +1,6 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
-import { injectAxe, checkA11y } from 'axe-playwright';
+import { injectAxe, checkA11y, getViolations } from 'axe-playwright';
+import fs from 'fs'
 
 const config: TestRunnerConfig = {
   async preVisit(page) {
@@ -11,13 +12,30 @@ const config: TestRunnerConfig = {
     const elementHandler = await page.$('#storybook-root');
     const innerHTML = await elementHandler?.innerHTML();
     expect(innerHTML).toMatchSnapshot();
-
-    // await checkA11y(page, '#storybook-root', {
-    //   detailedReport: true,
-    //   detailedReportOptions: {
-    //     html: true,
-    //   },
+    // const violations = await getViolations(page, '#storybook-root', {
+    //   reporter: 'html'
     // });
+  
+    // Do something with violations
+    // For example, write them to a file
+    // if(violations.length > 0) {
+    //   await new Promise<void>((resolve, reject) => {
+    //     fs.writeFile(
+    //       process.cwd() + `/results/${context.id}.json`,
+    //       JSON.stringify(violations, null, 2),
+    //       (err) => {
+    //         if (err) reject(err);
+    //         resolve();
+    //       }
+    //       );
+    //   });
+    // }
+    await checkA11y(page, '#storybook-root', {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true,
+      },
+    });
   },
 };
 
