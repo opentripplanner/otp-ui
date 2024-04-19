2084689a63ff260fd5a43841d6e435b285c12c68
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-04-19 10:22:28 -0700

    extend wait time before capturing

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -8,7 +8,7 @@ const config: TestRunnerConfig = {
   },
   async postVisit(page, context) {
     // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const elementHandler = await page.$('#storybook-root');
     const innerHTML = await elementHandler?.innerHTML();
     expect(innerHTML).toMatchSnapshot();
