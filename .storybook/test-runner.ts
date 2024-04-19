0ccaa0782a9de15dbd259bf48334fe26055c3cf6
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-04-19 10:37:30 -0700

    allow a11y and snapshot tests to be run separately

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,6 +1,23 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
-import { injectAxe, checkA11y, getViolations } from 'axe-playwright';
-import fs from 'fs'
+import { injectAxe, checkA11y } from 'axe-playwright';
+import { Page } from 'playwright-core'
+
+const ONLY_RUN = process.env.ONLY_RUN
+
+async function runSnapshots(page: Page) {
+  const elementHandler = await page.$('#storybook-root');
+  const innerHTML = await elementHandler?.innerHTML();
+  expect(innerHTML).toMatchSnapshot();
+}
+
+async function runA11yTest(page: Page) {
+  await checkA11y(page, '#storybook-root', {
+    detailedReport: true,
+    detailedReportOptions: {
+      html: true,
+    },
+  });
+}
 
 const config: TestRunnerConfig = {
   async preVisit(page) {
@@ -9,33 +26,12 @@ const config: TestRunnerConfig = {
   async postVisit(page, context) {
     // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    const elementHandler = await page.$('#storybook-root');
-    const innerHTML = await elementHandler?.innerHTML();
-    expect(innerHTML).toMatchSnapshot();
-    // const violations = await getViolations(page, '#storybook-root', {
-    //   reporter: 'html'
-    // });
-  
-    // Do something with violations
-    // For example, write them to a file
-    // if(violations.length > 0) {
-    //   await new Promise<void>((resolve, reject) => {
-    //     fs.writeFile(
-    //       process.cwd() + `/results/${context.id}.json`,
-    //       JSON.stringify(violations, null, 2),
-    //       (err) => {
-    //         if (err) reject(err);
-    //         resolve();
-    //       }
-    //       );
-    //   });
-    // }
-    await checkA11y(page, '#storybook-root', {
-      detailedReport: true,
-      detailedReportOptions: {
-        html: true,
-      },
-    });
+    if (!ONLY_RUN || ONLY_RUN === "SNAPSHOTS") {
+      await runSnapshots(page);
+    }
+    if (!ONLY_RUN || ONLY_RUN === "A11Y") {
+      await runA11yTest(page);
+    }
   },
 };
 
