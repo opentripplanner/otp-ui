45338ad1436f72b2e8f9cee20adf3bbe34c11067
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-11-11 15:17:16 -0800

    add react-intl decorators

--- packages/from-to-location-picker/src/index.story.tsx
+++ packages/from-to-location-picker/src/index.story.tsx
@@ -6,59 +6,42 @@ import FromToLocationPicker from ".";
 
 import "../__mocks__/trimet-mock.css";
 
+const withIntl = (Story: () => JSX.Element) => (
+  <IntlProvider messages={{}} locale="en">
+    <Story />
+  </IntlProvider>
+);
+
 export default {
   title: "From-To-Picker",
-  component: FromToLocationPicker
+  component: FromToLocationPicker,
+  decorators: [withIntl]
 };
 
 const onFromClick = action("onFromClick");
 const onToClick = action("onToClick");
 
 export const fromTo = (): ReactElement => (
-  <IntlProvider
-    locale="en-US"
-    messages={{
-      "otpUi.FromToLocationPicker.from": "Start here",
-      "otpUi.FromToLocationPicker.to": "Go here"
-    }}
-  >
-    <FromToLocationPicker onFromClick={onFromClick} onToClick={onToClick} />
-  </IntlProvider>
+  <FromToLocationPicker onFromClick={onFromClick} onToClick={onToClick} />
 );
 
 export const smallTextSansSerif = (): ReactElement => (
-  <IntlProvider
-    locale="en-US"
-    messages={{
-      "otpUi.FromToLocationPicker.from": "Start here",
-      "otpUi.FromToLocationPicker.to": "Go here"
-    }}
-  >
-    <span style={{ fontSize: "75%", fontFamily: "sans-serif" }}>
-      <FromToLocationPicker
-        label
-        onFromClick={onFromClick}
-        onToClick={onToClick}
-      />
-    </span>
-  </IntlProvider>
+  <span style={{ fontSize: "75%", fontFamily: "sans-serif" }}>
+    <FromToLocationPicker
+      label
+      onFromClick={onFromClick}
+      onToClick={onToClick}
+    />
+  </span>
 );
 
 export const customStyleAndText = (): React.ReactElement => (
-  <IntlProvider
-    locale="en-US"
-    messages={{
-      "otpUi.FromToLocationPicker.from": "Start here",
-      "otpUi.FromToLocationPicker.to": "Go here"
-    }}
-  >
-    <div className="trimet-ambient">
-      <FromToLocationPicker
-        label={<i style={{ color: "#ffff00" }}>Your trip:</i>}
-        onFromClick={onFromClick}
-        onToClick={onToClick}
-        showIcons={false}
-      />
-    </div>
-  </IntlProvider>
+  <div className="trimet-ambient">
+    <FromToLocationPicker
+      label={<i style={{ color: "#ffff00" }}>Your trip:</i>}
+      onFromClick={onFromClick}
+      onToClick={onToClick}
+      showIcons={false}
+    />
+  </div>
 );
