98377e0461ac25fbd0f36d9bd0dad0fb7de73a90
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2025-03-14 16:24:59 -0400

    Revert "refactor(itinerary-body): Extract method for computing leg destination"
    This reverts commit a242ac1259734126d58cf9cfb0a9bc13f69b2f99.

--- packages/itinerary-body/src/ItineraryBody/place-row.tsx
+++ packages/itinerary-body/src/ItineraryBody/place-row.tsx
@@ -36,18 +36,6 @@ function getLegPlaceName(
   };
 }
 
-function getLegDestination(
-  leg: Leg,
-  nextLeg: Leg,
-  PlaceName: FunctionComponent<PlaceNameProps>,
-  config: Config
-) {
-  const { placeName: nextPlaceName = undefined } = nextLeg
-    ? getLegPlaceName(nextLeg, false, PlaceName, config)
-    : {};
-  return nextPlaceName || <PlaceName config={config} place={leg.to} />;
-}
-
 /*
   TODO: Wondering if it's possible for us to destructure the time
   preferences from the config object and avoid making the props list so long
@@ -99,6 +87,12 @@ export default function PlaceRow({
     isDestination,
     PlaceName,
     config
+  );
+  const { placeName: nextPlaceName = undefined } = nextLeg
+    ? getLegPlaceName(nextLeg, false, PlaceName, config)
+    : {};
+  const legDestination = nextPlaceName || (
+    <PlaceName config={config} place={leg.to} />
   );
 
   // OTP2 marks both bikes and scooters as BIKESHARE in the vertextype
@@ -181,12 +175,7 @@ export default function PlaceRow({
               alwaysCollapseAlerts={alwaysCollapseAlerts}
               defaultFareSelector={defaultFareSelector}
               leg={leg}
-              legDestination={getLegDestination(
-                leg,
-                nextLeg,
-                PlaceName,
-                config
-              )}
+              legDestination={legDestination}
               LegIcon={LegIcon}
               legIndex={legIndex}
               nextLegInterlines={nextLeg?.interlineWithPreviousLeg}
