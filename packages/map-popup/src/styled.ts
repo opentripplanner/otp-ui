5416e5928c1cfef4ccec0de969ecc9aa9f4ff6c8
Josh Willis <168561922+josh-willis-arcadis@users.noreply.github.com>
2025-05-30 15:28:24 -0500

    fix(map-popup): remove stop id

--- packages/map-popup/src/styled.ts
+++ packages/map-popup/src/styled.ts
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 /* eslint-disable-next-line import/prefer-default-export */
-export const ViewStopButton = styled.button`
+export const ViewStopButton = styled.button<{ stopId?: string }>`
   background: none;
   border-bottom: none;
-  border-left: 1px solid #000;
+  ${props =>
+    props.stopId != null
+      ? "border-left: 1px solid #000; margin-left: 5px;"
+      : "border-left: none"};
   border-right: none;
   border-top: none;
   color: #008;
   font-family: inherit;
-  margin-left: 5px;
   padding-top: 0;
 `;
