da96dce1a18772f855bbf7ccb4228d0aa2e3c145
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2025-07-15 16:26:07 -0400

    refactor(RouteViewerOverlay): Revert sorting bound before fitBounds

--- packages/route-viewer-overlay/src/index.tsx
+++ packages/route-viewer-overlay/src/index.tsx
@@ -244,9 +244,7 @@ const RouteViewerOverlay = (props: Props): JSX.Element => {
     if (objectExistsAndPopulated(bounds) && current) {
       // Try to fit the map to route bounds immediately. If other overlays are still populating contents
       // and/or the map skips/aborts fitting for any reason, try fitting bounds again after a short delay.
-      const fitBounds = () =>
-        // eslint-disable-next-line no-underscore-dangle
-        util.fitMapToPoints(current, bounds._sw, bounds._ne);
+      const fitBounds = () => util.fitMapBounds(current, bounds);
       fitBounds();
       timeout = setTimeout(fitBounds, 250);
       if (props.mapCenterCallback) {
