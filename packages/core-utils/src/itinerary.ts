e810458c36e4ac3a4eec36f2ca0d7c0253ac8c4b
miles-grant-ibigroup <miles.grant@ibigroup.com>
2025-08-27 08:38:46 -0400

    feat(core-utils): use `stopCalls` to detect flex in leg

--- packages/core-utils/src/itinerary.ts
+++ packages/core-utils/src/itinerary.ts
@@ -98,12 +98,20 @@ export function legDropoffRequiresAdvanceBooking(leg: Leg): boolean {
  * returned by OTP when a leg is a flex leg.
  */
 export function isFlex(leg: Leg): boolean {
+  const callTypes =
+    leg?.stopCalls?.reduce((acc, cur) => {
+      // eslint-disable-next-line no-underscore-dangle
+      const callType = cur?.stopLocation?.__typename;
+      if (!acc.includes(callType)) {
+        acc.push(callType);
+      }
+
+      return acc;
+    }, []) || [];
   return (
-    isReservationRequired(leg) ||
-    isCoordinationRequired(leg) ||
-    legDropoffRequiresAdvanceBooking(leg) ||
-    isAdvanceBookingRequired(leg?.pickupBookingInfo) ||
-    legContainsGeometry(leg)
+    // This isn't the most efficient way to do this but it's so clean and there will
+    // only ever be 3 items in the array
+    callTypes.includes("LocationGroup") || callTypes.includes("Location")
   );
 }
 export function isRideshareLeg(leg: Leg): boolean {
