6f75dbdcb9610e32ca0920ca9f346793d6712fbe
Josh Willis <168561922+josh-willis-arcadis@users.noreply.github.com>
2025-09-23 14:10:33 -0500

    refactor(map-popup): remove unneeded type and type check

--- packages/map-popup/src/styled.ts
+++ packages/map-popup/src/styled.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 /* eslint-disable-next-line import/prefer-default-export */
-export const ViewStopButton = styled.button<{ stopId?: string | boolean }>`
+export const ViewStopButton = styled.button<{ stopId?: string }>`
   background: none;
   border-bottom: none;
   ${props =>
-    typeof props.stopId === "string"
+    props.stopId
       ? "border-left: 1px solid #000; margin-left: 5px;"
       : "border-left: none; padding-left: 0;"};
   border-right: none;
