6949039972cf4922265764e7e512ca3f5765d75c
danielhep <dh@danielheppner.com>
2025-12-09 13:22:35 -0800

    fix script problems

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,5 +1,5 @@
-import collectI18nMessages from "./collect-i18n-messages";
-import validateI18n from "./validate-i18n";
+import collectI18nMessages from "./collect-i18n-messages.js";
+import validateI18n from "./validate-i18n.js";
 
 export default {
   collectI18nMessages,
