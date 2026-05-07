4dc21755a01200582917dc2ceee1dde37918b99f
miles-grant-ibigroup <miles.grant@ibigroup.com>
2026-05-06 20:32:10 -0400

    run the formatter

--- packages/geocoder/src/geocoders/types.ts
+++ packages/geocoder/src/geocoders/types.ts
@@ -27,7 +27,7 @@ export type GeocoderConfig = {
   sources?: string;
   size?: number;
   reverseUseFeatureCollection?: boolean;
-  enableSlowFullUnicodeSupport?: boolean
+  enableSlowFullUnicodeSupport?: boolean;
 };
 
 export type ReverseQuery = {
