de16db22975fb3c86ff7a96d90cbfced5683c0f3
Alec Georgoff <alec.georgoff@gmail.com>
2026-07-15 09:25:03 -0700

    Adjust margin

--- packages/itinerary-body/src/ItineraryBody/place-row.tsx
+++ packages/itinerary-body/src/ItineraryBody/place-row.tsx
@@ -16,7 +16,7 @@ import { defaultMessages } from "../util";
 
 const CanceledTripMessage = styled.span`
   color: ${colors.red[700]};
-  margin-left: 5px;
+  margin-left: 7px;
   padding-top: 3px;
 `;
 
