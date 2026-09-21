eda3625269c91c359c8a77833dfed7f8e8f09d03
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-09-21 10:34:01 -0400

    test(otp2-tile-layer): Add story showing PHL tiles

--- packages/otp2-tile-overlay/src/mocks/handlers.js
+++ packages/otp2-tile-overlay/src/mocks/handlers.js
@@ -3,21 +3,21 @@ import { http } from "msw";
 
 import tilejson from "./tilejson.json";
 
-const seventy = new URL("./4770-6206.pbf", import.meta.url);
-const seventyOne = new URL("./4771-6206.pbf", import.meta.url);
-const seventyTwo = new URL("./4772-6206.pbf", import.meta.url);
+const tile47706206 = new URL("./4770-6206.pbf", import.meta.url);
+const tile47716206 = new URL("./4771-6206.pbf", import.meta.url);
+const tile47726206 = new URL("./4772-6206.pbf", import.meta.url);
 
 export default [
   http.get(
-    "https://fake-otp-server.com/otp/routers/default/vectorTiles/stops/tilejson.json",
+    "http://localhost:5555/otp/routers/default/vectorTiles/stops/tilejson.json",
     () => {
       return new Response(JSON.stringify(tilejson));
     }
   ),
   http.get(
-    "https://fake-otp-server.com/otp/routers/default/vectorTiles/stops/14/4770/6206.pbf",
+    "http://localhost:5555/otp/routers/default/vectorTiles/stops/14/4770/6206.pbf",
     async () => {
-      const buffer = await fetch(seventy).then(resp => resp.arrayBuffer());
+      const buffer = await fetch(tile47706206).then(resp => resp.arrayBuffer());
       return new Response(buffer, {
         headers: {
           "Content-Length": buffer.byteLength.toString(),
@@ -27,9 +27,9 @@ export default [
     }
   ),
   http.get(
-    "https://fake-otp-server.com/otp/routers/default/vectorTiles/stops/14/4771/6206.pbf",
+    "http://localhost:5555/otp/routers/default/vectorTiles/stops/14/4771/6206.pbf",
     async () => {
-      const buffer = await fetch(seventyOne).then(resp => resp.arrayBuffer());
+      const buffer = await fetch(tile47716206).then(resp => resp.arrayBuffer());
       return new Response(buffer, {
         headers: {
           "Content-Length": buffer.byteLength.toString(),
@@ -40,9 +40,9 @@ export default [
   ),
 
   http.get(
-    "https://fake-otp-server.com/otp/routers/default/vectorTiles/stops/14/4772/6206.pbf",
+    "http://localhost:5555/otp/routers/default/vectorTiles/stops/14/4772/6206.pbf",
     async () => {
-      const buffer = await fetch(seventyTwo).then(resp => resp.arrayBuffer());
+      const buffer = await fetch(tile47726206).then(resp => resp.arrayBuffer());
       return new Response(buffer, {
         headers: {
           "Content-Length": buffer.byteLength.toString(),
