f4a5ca8cc3d4438ff4340a5edc75cc38e224a22b
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-09-21 15:46:45 -0400

    test(otp2-tile-overlay): Extract tile fetch method in mock handlers

--- packages/otp2-tile-overlay/src/mocks/handlers.js
+++ packages/otp2-tile-overlay/src/mocks/handlers.js
@@ -11,90 +11,52 @@ const atl10891640 = new URL("./atl/1089-1640.pbf", import.meta.url);
 const phl47706205 = new URL("./phl/4770-6205.pbf", import.meta.url);
 const phl47716205 = new URL("./phl/4771-6205.pbf", import.meta.url);
 
+function fetchTile(tileLocalUrl) {
+  return async () => {
+    const buffer = await fetch(tileLocalUrl).then(resp => resp.arrayBuffer());
+    return new Response(buffer, {
+      headers: {
+        "Content-Length": buffer.byteLength.toString(),
+        "Content-Type": "application/x-protobuf"
+      }
+    });
+  };
+}
+
 export default [
+  // PHL stop tiles
   http.get(
     "http://localhost:5555/phl/otp/routers/default/vectorTiles/stops/tilejson.json",
-    () => {
-      return new Response(JSON.stringify(phlTilejson));
-    }
+    () => new Response(JSON.stringify(phlTilejson))
   ),
   http.get(
     "http://localhost:5555/phl/otp/routers/default/vectorTiles/stops/14/4770/6205.pbf",
-    async () => {
-      const buffer = await fetch(phl47706205).then(resp => resp.arrayBuffer());
-      return new Response(buffer, {
-        headers: {
-          "Content-Length": buffer.byteLength.toString(),
-          "Content-Type": "application/x-protobuf"
-        }
-      });
-    }
+    fetchTile(phl47706205)
   ),
   http.get(
     "http://localhost:5555/phl/otp/routers/default/vectorTiles/stops/14/4771/6205.pbf",
-    async () => {
-      const buffer = await fetch(phl47716205).then(resp => resp.arrayBuffer());
-      return new Response(buffer, {
-        headers: {
-          "Content-Length": buffer.byteLength.toString(),
-          "Content-Type": "application/x-protobuf"
-        }
-      });
-    }
+    fetchTile(phl47716205)
   ),
 
+  // ATL areaStop tiles
   http.get(
     "http://localhost:5555/atl/otp/routers/default/vectorTiles/areaStops/tilejson.json",
-    () => {
-      return new Response(JSON.stringify(atlTilejson));
-    }
+    () => new Response(JSON.stringify(atlTilejson))
   ),
   http.get(
     "http://localhost:5555/atl/otp/routers/default/vectorTiles/areaStops/12/1088/1639.pbf",
-    async () => {
-      const buffer = await fetch(atl10881639).then(resp => resp.arrayBuffer());
-      return new Response(buffer, {
-        headers: {
-          "Content-Length": buffer.byteLength.toString(),
-          "Content-Type": "application/x-protobuf"
-        }
-      });
-    }
+    fetchTile(atl10881639)
   ),
   http.get(
     "http://localhost:5555/atl/otp/routers/default/vectorTiles/areaStops/12/1088/1640.pbf",
-    async () => {
-      const buffer = await fetch(atl10881640).then(resp => resp.arrayBuffer());
-      return new Response(buffer, {
-        headers: {
-          "Content-Length": buffer.byteLength.toString(),
-          "Content-Type": "application/x-protobuf"
-        }
-      });
-    }
+    fetchTile(atl10881640)
   ),
   http.get(
     "http://localhost:5555/atl/otp/routers/default/vectorTiles/areaStops/12/1089/1639.pbf",
-    async () => {
-      const buffer = await fetch(atl10891639).then(resp => resp.arrayBuffer());
-      return new Response(buffer, {
-        headers: {
-          "Content-Length": buffer.byteLength.toString(),
-          "Content-Type": "application/x-protobuf"
-        }
-      });
-    }
+    fetchTile(atl10891639)
   ),
   http.get(
     "http://localhost:5555/atl/otp/routers/default/vectorTiles/areaStops/12/1089/1640.pbf",
-    async () => {
-      const buffer = await fetch(atl10891640).then(resp => resp.arrayBuffer());
-      return new Response(buffer, {
-        headers: {
-          "Content-Length": buffer.byteLength.toString(),
-          "Content-Type": "application/x-protobuf"
-        }
-      });
-    }
+    fetchTile(atl10891640)
   )
 ];
