7a795ac3885c52b094e421c47fd07a72a8d341ab
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-09-21 10:48:58 -0400

    test(otp2-tile-overlay): tidy up stories

--- packages/otp2-tile-overlay/src/Otp2TileOverlay.story.tsx
+++ packages/otp2-tile-overlay/src/Otp2TileOverlay.story.tsx
@@ -3,32 +3,26 @@ import BaseMap from "@opentripplanner/base-map";
 import generateOTP2TileLayers from ".";
 
 export default {
-  title: "OTP2 Tile Layer",
-  parameters: { storyshots: { disable: true } }
+  parameters: { storyshots: { disable: true } },
+  title: "OTP2 Tile Layer"
 };
 
 // TODO: Add a story to illustrate "color" prop passed from overlay.
 
-export const OtpTileLayer = (): JSX.Element => {
-  const [endpoint, setEndpoint] = useState("https://fake-otp-server.com/otp");
+export const OtpTileLayerWithYourOwnServer = (): JSX.Element => {
+  const [endpoint, setEndpoint] = useState("");
   return (
     <>
       {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
       <label>
-        OTP2 Server with stops layer enabled
+        OTP2 Server with stops layers enabled:{" "}
         <input
           onChange={e => setEndpoint(e.target.value)}
           placeholder="http://localhost:8001/otp"
           value={endpoint}
         />
       </label>
-      <pre style={{ display: "inline", paddingLeft: 5 }}>
-        https://fake-otp-server.com/otp
-      </pre>{" "}
-      mocks downtown Philadelphia stop tile data. Check stops in the layer
-      selector and zoom in on downtown Philadelphia (near city hall) to see some
-      stops loaded from OTP tiles.
-      <BaseMap center={[0, 0]} zoom={3} style={{ height: "80vh" }}>
+      <BaseMap center={[0, 0]} style={{ height: "80vh" }} zoom={3}>
         {generateOTP2TileLayers(
           [{ type: "stops" }, { type: "areaStops" }],
           `${endpoint}/routers/default/vectorTiles`
@@ -40,11 +34,11 @@ export const OtpTileLayer = (): JSX.Element => {
 
 export const OtpStopTileLayerPHL = (): JSX.Element => (
   <>
-    <code>http://localhost:5555/otp</code> mocks some stop tiles for downtown
+    <code>http://localhost:5555/otp</code> mocks some stop tiles in downtown
     Philadelphia (near City Hall).
-    <BaseMap center={[39.9526, -75.1652]} zoom={14} style={{ height: "80vh" }}>
+    <BaseMap center={[39.9526, -75.1652]} style={{ height: "80vh" }} zoom={14}>
       {generateOTP2TileLayers(
-        [{ type: "stops", minZoom: 5, initiallyVisible: true }],
+        [{ initiallyVisible: true, minZoom: 5, type: "stops" }],
         "http://localhost:5555/otp/routers/default/vectorTiles"
       )}
     </BaseMap>
